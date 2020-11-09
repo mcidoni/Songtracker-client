6fef2cf6d589b3660ff7126f0d70ea5d5ad1df6a
mcidoni <matthewcidoni@gmail.com>
2020-11-09 14:20:18 -0500

    bulk commit for folder structure and account modifiers

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,16 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const events = require('.././scripts/events')
 
 $(() => {
-  // your JS code goes here
+
+  // POST sign-up
+  $('#sign-up-form').on('submit', events.onSignUp)
+  // POST sign-in
+  $('#sign-in-form').on('submit', events.onSignIn)
+  // PATCH change-password
+  $('#change-password-form').on('submit', events.onChangePassword)
+  // DELETE sign-out
+  $('#sign-out').on('click', events.onSignOut)
 })
---
+++ assets/scripts/events.js
@@ -0,0 +1,51 @@
+'use strict'
+const api = require('./api')
+const ui = require('./ui')
+const getFormFields = require('../../../lib/get-form-fields')
+// const store = require('./store')
+
+const onSignUp = e => {
+  e.preventDefault()
+  const form = e.target
+  const data = getFormFields(form)
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
+}
+
+const onSignIn = e => {
+  e.preventDefault()
+  const form = e.target
+  const formData = getFormFields(form)
+  api.signIn(formData)
+    .then(ui.signInSuccess)
+    .then(() => {
+      api.createGame()
+        .then(ui.createGameSuccess)
+        .catch(ui.createGameFailure)
+    })
+    .catch(ui.signInFailure)
+}
+
+const onChangePassword = e => {
+  e.preventDefault()
+  const form = e.target
+  const formData = getFormFields(form)
+  api.changePassword(formData)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
+}
+
+const onSignOut = e => {
+  e.preventDefault()
+  api.signOut()
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+}
+
+module.exports = {
+  onSignUp,
+  onSignIn,
+  onChangePassword,
+  onSignOut
+}
